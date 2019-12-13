aad80326d0fcb0b46b9ba1787fef05a0e625e720
keziah frye <malachi.j.frye@gmail.com>
2019-12-12 22:05:15 -0800

    initial comit

--- app/classes/text.py
+++ app/classes/text.py
@@ -1,4 +1,14 @@
 # when given a string, return the length of the string
 
-
+# when given a STRING
+def stringlength(string):
+# COUNT the CHAR in the STRING
+    counter = 0 
+    for char in string: 
+        counter = counter + 1 
+         
+# return COUNT
+    return counter 
 # when given a string and a number in the string, return the charcter 
+result = stringlength("Debug me")
+print(result)
